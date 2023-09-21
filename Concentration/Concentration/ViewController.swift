18c22ffd870a4a32c1b4c219fc69fbc607a40d0e
twrkkk <zhenya.shein.03@gmail.com>
2023-09-21 08:28:45 -0700

    ypa

---
+++ Concentration/Concentration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Жокей on 21.09.2023.
+//  Copyright © 2023 Жокей. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
