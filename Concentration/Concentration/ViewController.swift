7996bbb95e3015cc2ed5a2c1c665f5835c4a196c
twrkkk <zhenya.shein.03@gmail.com>
2023-09-21 11:21:37 -0700

    first lection completed

--- Concentration/Concentration/ViewController.swift
+++ Concentration/Concentration/ViewController.swift
@@ -9,12 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    @IBAction func TouchCard(_ sender: UIButton) {
+        FlipCard(withEmoji: "🍒", on: sender)
     }
-
-
+    
+    func FlipCard(withEmoji emoji: String, on button: UIButton){
+        if button.currentTitle == emoji{
+            button.setTitle("", for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)
+        } else{
+            button.setTitle(emoji, for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+        
+    }
 }
 
